53e001d38941bb4944cd2cdf6a58d256ace17a0a
manyhf16 <manyhf16@163.com>
2016-09-11 21:30:55 +0800

    test

--- yihang-ssm/src/test/java/org/yihang/demo/Test.java
+++
@@ -1,7 +0,0 @@
-package org.yihang.demo;
-
-/**
- * Created by lenovo on 2016/9/11.
- */
-public class Test {
-}
---
+++ yihang-ssm/src/test/java/org/yihang/demo/TestGit.java
@@ -0,0 +1,13 @@
+package org.yihang.demo;
+
+import org.junit.Test;
+
+/**
+ * Created by yihang on 2016/9/11.
+ */
+public class TestGit {
+    @Test
+    public void test() {
+        System.out.println("ok");
+    }
+}
