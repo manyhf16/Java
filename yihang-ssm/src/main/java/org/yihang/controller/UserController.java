29beb33ea12c37fd00018d0a6ea9c73213110942
manyhf16 <manyhf16@163.com>
2016-09-16 11:34:55 +0800

    test

--- yihang-ssm/src/main/java/org/yihang/controller/UserController.java
+++ yihang-ssm/src/main/java/org/yihang/controller/UserController.java
@@ -3,19 +3,22 @@ package org.yihang.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.yihang.entity.User;
+
+import java.util.Date;
 
 @Controller
 public class UserController {
-	
-	@RequestMapping("/jsp.do")
-	public void jsp() {
-	}
-	
-	@RequestMapping(value="/string.do", produces="text/plain;charset=utf-8")
+
+	@RequestMapping("/user")
 	@ResponseBody
-	public String string() {
-		System.out.println("-------------");
-		return "您好";
+	public User user() {
+		User u = new User();
+		u.setId(1);
+		u.setUsername("张三");
+		u.setPassword("123");
+		u.setUpdatetime(new Date());
+		return u;
 	}
 
 }
